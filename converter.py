cb4482e1dea7ba44fd5751bd315db2eaf2a687c7
Anna Beatriz <annabea@icomp.ufam.edu.br>
2023-10-10 21:56:19 -0400

    first commit

---
+++ converter.py
@@ -0,0 +1,14 @@
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5.0/9.0
+    return celsius
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9.0/5.0) + 32
+    return fahrenheit
+
+
+#celsius = fahrenheit_to_celsius(fahrenheit)
+#print(f"{fahrenheit} graus Fahrenheit são equivalentes a {celsius:.2f} graus Celsius.")
+
+#fahrenheit = celsius_to_fahrenheit(celsius)
+#print(f"{celsius} graus Celsius são equivalentes a {fahrenheit:.2f} graus Fahrenheit.")
