207a4772d283c29250fee7ea558b0313db8ce6dc
Anna Beatriz <annabea@icomp.ufam.edu.br>
2023-10-12 23:07:11 -0400

    alteracoes 15

--- testes.py
+++ testes.py
@@ -4,7 +4,7 @@ from converter import fahrenheit_para_celsius, celsius_para_fahrenheit
 class TestConversorTemperatura(unittest.TestCase):
     def test_conversao_fahrenheit_para_celsius(self):
         self.assertEqual(fahrenheit_para_celsius(32), 0)
-        self.assertEqual(fahrenheit_para_celsius(212), 0)
+        self.assertEqual(fahrenheit_para_celsius(212), 100)
         self.assertEqual(fahrenheit_para_celsius(68), 20)
 
     def test_conversao_celsius_para_fahrenheit(self):
